d6870b884c91e98747590961b0888445598ce086
Keoni Gandall <koeng101@gmail.com>
2019-04-16 14:13:05 -0700

    ultimate simplicity

--- app/config.py
+++
@@ -1,9 +0,0 @@
-#import boto3
-import os
-SECRET_KEY = os.environ['SECRET_KEY']
-URL = os.environ['URL']
-DEV = os.environ['DEV']
-LOGIN_KEY = os.environ['LOGIN_KEY']
-PREFIX = os.environ['PREFIX'] # BBF10K_
-
-
