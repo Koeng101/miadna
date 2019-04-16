b4f8963d9095eb2f6c5b0f4d307312da4c9041f4
Keoni Gandall <koeng101@gmail.com>
2019-04-16 13:50:50 -0700

    postgres_url to url

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 #import boto3
 import os
 SECRET_KEY = os.environ['SECRET_KEY']
-URL = os.environ['POSTGRES_URL']
+URL = os.environ['URL']
 DEV = os.environ['DEV']
 LOGIN_KEY = os.environ['LOGIN_KEY']
 PREFIX = os.environ['PREFIX'] # BBF10K_
