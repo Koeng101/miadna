09cacc6f18f4b6b7d0c81b139a6d221fc156b1b6
Keoni Gandall <koeng101@gmail.com>
2019-04-16 14:36:48 -0700

    added app

---
+++ app/config.py
@@ -0,0 +1,9 @@
+#import boto3
+import os
+SECRET_KEY = os.environ['SECRET_KEY']
+URL = os.environ['URL']
+DEV = os.environ['DEV']
+LOGIN_KEY = os.environ['LOGIN_KEY']
+PREFIX = os.environ['PREFIX'] # BBF10K_
+
+
